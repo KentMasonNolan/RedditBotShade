a7fa52f620a70011d74c41a2a9ab2fb74f06ea84
kent <kent.nolan@gmail.com>
2023-05-25 15:29:18 +1200

    Commit to get praw installed

--- Main.py
+++ Main.py
@@ -1,1 +1,14 @@
-print("Hello World")
+#!/usr/bin/python
+from uaclient import pip
+
+# pip import praw
+
+reddit = praw.Reddit('bot1')
+
+subreddit = reddit.subreddit("learnpython")
+
+for submission in subreddit.hot(limit=5):
+    print("Title: ", submission.title)
+    print("Text: ", submission.selftext)
+    print("Score: ", submission.score)
+    print("---------------------------------\n")
